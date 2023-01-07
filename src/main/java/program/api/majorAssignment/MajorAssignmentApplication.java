2469a9fafb7db0959b57fc6cd8fcf32d048e7473
thuan-utc <thuansl99@gmail.com>
2023-01-07 13:57:28 +0700

    initial project from start.spring.io

---
+++ src/main/java/program/api/majorAssignment/MajorAssignmentApplication.java
@@ -0,0 +1,13 @@
+package program.api.majorAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajorAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MajorAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/program/api/majorAssignment/MajorAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package program.api.majorAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
